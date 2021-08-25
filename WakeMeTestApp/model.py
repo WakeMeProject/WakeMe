5ff2d26b1195180143b5d2db340ce75b2467595b
Tanuj Sutaria <tanuj@Tanujs-MacBook-Pro.local>
2021-08-24 23:06:09 -0700

    cleaning up model file

--- WakeMeTestApp/model.py
+++ WakeMeTestApp/model.py
@@ -1,27 +1,10 @@
 from tensorflow import keras
-import matplotlib.pyplot as plt
 import tensorflow as tf
-
 from preprocessing import preprocess_dataset
-import WakeMeMeta
 
-model_path = "/Users/tanuj/PycharmProjects/WakeMe/WakeMeTestApp/models/fb_wake_test.h5"
+default_model_path = "/Users/tanuj/PycharmProjects/WakeMe/WakeMeTestApp/models/fb_wake_test.h5"
 
-model = keras.models.load_model(model_path)
-
-#
-# sample_file = "/Users/tanuj/PycharmProjects/WakeMe/WakeMeTestApp/test_audio_files/xx.wav"
-#
-# sample_ds = preprocess_dataset([str(sample_file)])
-#
-# for spectrogram, label in sample_ds.batch(1):
-#     prediction = model(spectrogram)
-#     np_label = tf.math.argmax(prediction[0]).numpy()
-#     print(WakeMeMeta.commands[np_label])
-#     plt.bar(WakeMeMeta.commands, tf.nn.softmax(prediction[0]))
-#     plt.title(f'Predictions for "{WakeMeMeta.commands[label[0]]}"')
-#     plt.show()
-def choose_model(model_file_path = model_path):
+def choose_model(model_file_path = default_model_path):
     global chosen_model
     chosen_model= ChosenModel(model_file_path)
     return chosen_model.model
